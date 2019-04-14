d09e186487724627ecd8bb1ba67c66ee784917e1
Krystian Woźniak <krys.blu@gmail.com>
2019-04-14 09:31:37 +0200

    Implement: ExchangeRateFilter filter by currency code

---
+++ src/main/java/pl/nbp/parser/exchangeRate/ExchangeRateFilter.java
@@ -0,0 +1,13 @@
+package pl.nbp.parser.exchangeRate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ExchangeRateFilter {
+
+    public static List<ExchangeRate> filterByCurrencyCode (List<ExchangeRate> exchangeRates, String currencyCode) {
+        return exchangeRates.stream()
+                .filter(e -> e.getCurrencyCode().equals(currencyCode))
+                .collect(Collectors.toList());
+    }
+}
